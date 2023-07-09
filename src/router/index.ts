c1bd920ff90871a4fb83e1ad1e0a552dedb5711c
ethanelliott <ethan.elliott@hotmail.ca>
2023-07-09 14:19:50 -0400

    chore: 🔧 pushing everything

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,7 +8,11 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
-    }
+    },
+    {
+      path: "/:pathMatch(.*)*",
+      redirect: "/",
+    },
   ],
 });
 
