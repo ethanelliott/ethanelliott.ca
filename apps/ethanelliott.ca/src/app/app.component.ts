0a9fdc8edcb45dca43019120e7d3c1d2bc077a24
Ethan Elliott <ethan.elliott@hotmail.ca>
2024-07-14 15:05:19 -0400

    yup

---
+++ apps/ethanelliott.ca/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  selector: 'ee-root',
+  template: `Hello`,
+  styles: ``,
+})
+export class AppComponent {}
