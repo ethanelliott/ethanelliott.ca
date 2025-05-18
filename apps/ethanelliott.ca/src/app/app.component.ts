59abde6353ef249ccd52a0fa4e9b10b2bcdeda07
Ethan Elliott <ethan.elliott@hotmail.ca>
2025-05-18 13:01:27 -0400

    ?

--- apps/ethanelliott.ca/src/app/app.component.ts
+++ apps/ethanelliott.ca/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { MatSlideToggle } from '@angular/material/slide-toggle';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [MatSlideToggle],
   selector: 'ee-root',
-  template: `Hello`,
+  template: `<p>Hello</p>
+    <mat-slide-toggle>Toggle me!</mat-slide-toggle> `,
   styles: ``,
 })
 export class AppComponent {}
