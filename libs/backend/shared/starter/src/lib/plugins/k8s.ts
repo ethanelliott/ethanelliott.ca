aac4b595a35a173ea0684789827d360cb64d07a3
Ethan Elliott <ethan.elliott@hotmail.ca>
2026-02-16 17:07:31 -0500

    chore: silence health check endpoint logging

--- libs/backend/shared/starter/src/lib/plugins/k8s.ts
+++ libs/backend/shared/starter/src/lib/plugins/k8s.ts
@@ -2,11 +2,11 @@ import { FastifyInstance } from 'fastify';
 import fp from 'fastify-plugin';
 
 export const K8sPlugin = fp(async function (fastify: FastifyInstance) {
-  fastify.get('/liveness', async (request, reply) => {
+  fastify.get('/liveness', { logLevel: 'silent' }, async (request, reply) => {
     reply.send({ live: true });
   });
 
-  fastify.get('/readiness', async (request, reply) => {
+  fastify.get('/readiness', { logLevel: 'silent' }, async (request, reply) => {
     return reply.send({ ready: true });
   });
 });
